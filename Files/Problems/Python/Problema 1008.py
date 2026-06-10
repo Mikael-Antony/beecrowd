152c255993410d893672d574bfcf4f8da32972dc
MikaelAntony <mikael11antony@gmail.com>
2026-06-10 08:56:18 -0300

    Create Problema 1008.py
    Write a program that reads an employee's number, their number of hours worked, their hourly rate, and calculates the employee's salary. Then, display the employee's number and salary, with two decimal places.

---
+++ Files/Problems/Python/Problema 1008.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+Num = int(input())
+HoraTrabalhada = int(input())
+SalarioHora = float(input())
+
+print(f"NUMBER = {Num}\nSALARY = U$ {(HoraTrabalhada * SalarioHora):.2f}")
